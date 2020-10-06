d255ab2d52d264a8148bdeb84271a61b0f456118
Muthoni2000 <bmuthoni39@yahoo.com>
2020-10-06 13:55:00 +0300

    create profile page for users

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,42 +1,41 @@
-from flask import render_template
 from . import auth
+from flask import render_template
 from ..models import User
 from flask import render_template, redirect, url_for
 from .forms import LoginForm,RegistrationForm
-from flask_login import login_user, logout_user,login_required
-from .. import db
-
-
-@auth.route('/login')
-@auth.route('/register', methods = ['GET', 'POST'])
+@@ -11,12 +11,16 @@
 def register():
      form = RegistrationForm()
      if form.validate_on_submit():
-         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
-         db.session.add(user)
-         db.session.commit()
-         return redirect(url_for('auth.login'))
-         title = "Sign Up"
-     return render_template('auth/register.html', registration_form = form)
+                name = form.username.data
+        email = form.email.data
+        pass_code = form.password.data
+        profile_pic = "default.png"
+        bio = "No bio"
+        new_user = User(name = name, email = email, password = pass_code ,profile_pic = profile_pic, bio = bio)
+        new_user.save_user()
+        title = "Sign Up"
+     return render_template('auth/register.html', registration_form = form, title = title)
+
 
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('auth/login.html') 
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
             return redirect(request.args.get('next')or url_for('main.index'))
-
         flash('Invalid username or password')
-
     title = 'LOG IN'
     return render_template('auth/login.html', login_form = login_form, title = title)
+
+
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash("Successful Log Out")
     return redirect(url_for("main.index")) 
--- app/models.py
+++ app/models.py
@@ -2,21 +2,19 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(225))
-    email = db.Column(db.String(225), unique = True, index = True)
-    pass_secure = db.Column(db.String(225))
-    pitches = db.relationship('Pitch', backref = 'pitch', lazy = "dynamic")
     name = db.Column(db.String(225))
     email = db.Column(db.String)
     bio = db.Column(db.String)
     profile_pic  = db.Column(db.String)
+    pass_code = db.Column(db.String)
 
     pitches = db.relationship('Pitch', backref = 'name', lazy = "dynamic")
     comments = db.relationship('Comment', backref = 'name', lazy = "dynamic")
-
+   
 
 
     def save_user(self):
@@ -32,16 +30,15 @@ class User(UserMixin, db.Model):
 
 
     @property
-@@ -18,30 +34,57 @@ def password(self):
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
 
-    @password.setter
+   @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
         self.pass_code = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
         return check_password_hash(self.pass_code,password)
 
 
@@ -51,7 +48,6 @@ class User(UserMixin, db.Model):
 
 
     def __repr__(self):
-        return f'User {self.username}'
         return f'User {self.name}'
 
 
@@ -59,41 +55,46 @@ class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key = True)
-    pitch_title = db.Column(db.String(225))
-    pitch_content = db.Column(db.String(225))
-    pitch_category = db.Column(db.String(225))
     category = db.Column(db.String)
     title = db.Column(db.String) 
     content = db.Column(db.String)
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
 
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    comments = db.relationship('Comment', backref = 'pitch_id', lazy = "dynamic")
+    
+
 
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
-
     @classmethod
     def get_pitches(cls, category):
         pitches_list = Pitch.query.filter_by(category = category)
         return pitches_list
 
+    @classmethod
+    def pitches_count(cls,username):
+        user = User.query.filter_by(name = username).first()
+        pitches = Pitch.query.filter_by(user_id=user_id).all()
+
+        pitch_count = 0
+        for pitch in pitches:
+            pitch_count += 1
+        return pitch_count
+
 
 
     def __repr__(self):
-        return f'User {self.username}'
         return f'User {self.name}'
-
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     mention = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
-
-
-
