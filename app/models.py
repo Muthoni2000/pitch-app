85f881c331b86df95aee278a80c4366651c1942e
Muthoni2000 <bmuthoni39@yahoo.com>
2020-10-07 01:04:21 +0300

    create a get_comment function and view_com

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.utils import secure_filename
 from flask_login import UserMixin
 from . import login_manager
 
@@ -14,8 +15,8 @@ class User(UserMixin, db.Model):
 
     pitches = db.relationship('Pitch', backref = 'name', lazy = "dynamic")
     comments = db.relationship('Comment', backref = 'name', lazy = "dynamic")
-   
 
+   
 
     def save_user(self):
         db.session.add(self)
@@ -33,7 +34,7 @@ class User(UserMixin, db.Model):
     def password(self):
         raise AttributeError("You cannot read the password attribute")
 
-   @password.setter
+    @password.setter
     def password(self, password):
         self.pass_code = generate_password_hash(password)
 
@@ -42,13 +43,16 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.pass_code,password)
 
 
-return User.query.get(user_id)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
 
     def __repr__(self):
         return f'User {self.name}'
-        
-        class Pitch(db.Model):
+
+
+class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -57,44 +61,65 @@ return User.query.get(user_id)
     content = db.Column(db.String)
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
-    
+
+    comments = db.relationship("Comment", backref = "pitch", lazy = "dynamic")
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     
-    def save_pitch(self):
-        db.session.add(self)
-        db.session.commit()
-
 
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
+
     @classmethod
     def get_pitches(cls, category):
         pitches_list = Pitch.query.filter_by(category = category)
         return pitches_list
 
-    @classmethod
-    def pitches_count(cls,username):
-        user = User.query.filter_by(name = username).first()
-        pitches = Pitch.query.filter_by(user_id=user_id).all()
 
-        pitch_count = 0
-        for pitch in pitches:
-            pitch_count += 1
-        return pitch_count
+    def likes(self):
+        self.likes = self.likes + 1
+        self.vote_count = self.likes - self.dislikes
+        db.session.add(self)
+        db.session.commit()
 
+    def dislikes(self):
+        self.dislikes = self.dislikes + 1
+        self.vote_count = self.likes - self.dislikes
+        db.session.add(self)
+        db.session.commit()
 
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f'Pitch {self.title}'
+
+
+
+
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     mention = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
 
+
+
+    @classmethod
+    def get_comments_by_pitch(cls,id):
+        '''
+        function to retrieve comments based on the pitch_id
+        '''
+        comments_list = Comments.query.filter_by(pitch_id = id).all()
+
+        return comments_list
 
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
+
+    
+    def __repr__(self):
+        return f'Comment {self.comment}'
+
+    
